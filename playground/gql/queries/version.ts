cc17f6a9cdd153f4c44c8edadc05ec5809eddf2b
Benjamin VIELLARD <gbicou@gmail.com>
2023-02-01 17:50:13 +0100

    prettier eslint

--- playground/gql/queries/version.ts
+++ playground/gql/queries/version.ts
@@ -1,13 +1,18 @@
-import type * as Types from '../types';
+import type * as Types from "../types";
 
-import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type VersionQueryVariables = Types.Exact<{ [key: string]: never; }>;
+import type { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+export type VersionQueryVariables = Types.Exact<{ [key: string]: never }>;
 
+export type VersionQuery = { __typename?: "Query" } & Pick<Types.Query, "version">;
 
-export type VersionQuery = (
-  { __typename?: 'Query' }
-  & Pick<Types.Query, 'version'>
-);
-
-
-export const VersionDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"version"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"version"}}]}}]} as unknown as DocumentNode<VersionQuery, VersionQueryVariables>;
+export const VersionDocument = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "query",
+      name: { kind: "Name", value: "version" },
+      selectionSet: { kind: "SelectionSet", selections: [{ kind: "Field", name: { kind: "Name", value: "version" } }] },
+    },
+  ],
+} as unknown as DocumentNode<VersionQuery, VersionQueryVariables>;
