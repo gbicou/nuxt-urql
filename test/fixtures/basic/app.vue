ebfbc6951a1080873864a3bc81ee5bda43f26dc7
Benjamin VIELLARD <gbicou@gmail.com>
2023-02-02 15:28:30 +0100

    test: actually test a graphql query

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>basic</div>
+  <div>
+    <ul>
+      <li v-for="c of countries" :key="c">
+        {{ c.name }}
+      </li>
+    </ul>
+    basic
+  </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { computed, useQuery } from "#imports";
+import { gql } from "@urql/core";
+
+const { data } = await useQuery({query: gql`query countries {countries { name }}`})
+const countries = computed(() => data.value?.countries ?? [])
+</script>
