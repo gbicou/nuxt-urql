7cf680da21000f24605dae56d2860400b03f27c6
Benjamin VIELLARD <bicou@bicou.com>
2023-02-03 00:09:45 +0100

    refactor(schema): extract sample schema from playground

--- playground/server/api/graphql.ts
+++ playground/server/api/graphql.ts
@@ -1,37 +1,5 @@
 import { createYoga } from "graphql-yoga";
-import SchemaBuilder from "@pothos/core";
-import { version } from "../../../package.json";
-import { records, type ISO_3166_1_Record } from "iso-3166-1-ts";
-import { defineEventHandler } from "h3";
-
-// build schema with pothos
-const builder = new SchemaBuilder({});
-
-const Country = builder.objectRef<ISO_3166_1_Record>("Country");
-
-builder.objectType(Country, {
-  description: "A country record",
-  fields: (t) => ({
-    code: t.exposeString("alpha2", { description: "ISO code" }),
-    name: t.exposeString("name", { description: "Name of country" }),
-  }),
-});
-
-builder.queryType({
-  fields: (t) => ({
-    version: t.string({
-      description: "Package version",
-      resolve: () => version,
-    }),
-    countries: t.field({
-      type: [Country],
-      description: "All countries",
-      resolve: () => records,
-    }),
-  }),
-});
-
-export const schema = builder.toSchema();
+import { schema } from "@bicou/nuxt-urql-sample-schema/schema";
 
 // Create a Yoga instance with a GraphQL schema.
 const yoga = createYoga({
---
+++ sample-schema/schema.ts
@@ -0,0 +1,32 @@
+import SchemaBuilder from "@pothos/core";
+import { version } from "../package.json";
+import { records, type ISO_3166_1_Record } from "iso-3166-1-ts";
+
+// build schema with pothos
+const builder = new SchemaBuilder({});
+
+const Country = builder.objectRef<ISO_3166_1_Record>("Country");
+
+builder.objectType(Country, {
+  description: "A country record",
+  fields: (t) => ({
+    code: t.exposeString("alpha2", { description: "ISO code" }),
+    name: t.exposeString("name", { description: "Name of country" }),
+  }),
+});
+
+builder.queryType({
+  fields: (t) => ({
+    version: t.string({
+      description: "Package version",
+      resolve: () => version,
+    }),
+    countries: t.field({
+      type: [Country],
+      description: "All countries",
+      resolve: () => records,
+    }),
+  }),
+});
+
+export const schema = builder.toSchema();
