20d73830cbbb0a0357c64fa06b5a4ddbe583e491
Benjamin VIELLARD <bicou@bicou.com>
2023-02-04 14:36:03 +0100

    docs: base edition

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,18 +1,21 @@
 export default defineAppConfig({
   docus: {
-    title: "Docus",
+    github: {
+      repo: "gbicou/nuxt-urql"
+    },
+    title: "Nuxt URQL",
     description: "The best place to start your documentation.",
     image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
-      twitter: "nuxt_themes",
-      github: "nuxt-themes/docus",
+      // twitter: "nuxt_themes",
+      github: "gbicou/nuxt-urql",
     },
     aside: {
       level: 0,
       exclude: [],
     },
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
     },
