bcab386ef8da6a6eeb07c5d311a5219d9e911926
Benjamin VIELLARD <gbicou@gmail.com>
2023-02-13 13:15:06 +0100

    docs: tsdoc

--- src/runtime/client.ts
+++ src/runtime/client.ts
@@ -2,10 +2,18 @@ import { dedupExchange, cacheExchange, fetchExchange, type ClientOptions } from 
 import { useRuntimeConfig } from "#app";
 import type { SSRExchange } from "@urql/core/dist/types/exchanges/ssr";
 
-// client options except endpoint
+/**
+ * client options except endpoint
+ *
+ * @see {@link ClientOptions} for the returned data structure
+ */
 export type UrqlClientOptions = Omit<ClientOptions, "url">;
 
-// helper to build client options from configured ssr
+/**
+ * helper to build client options from configured ssr
+ *
+ * @param ssr - exchange configured to work with nuxt payload
+ */
 export type UrqlClientBuild = (ssr: SSRExchange) => PromiseLike<UrqlClientOptions> | UrqlClientOptions;
 
 // helper to define client options
