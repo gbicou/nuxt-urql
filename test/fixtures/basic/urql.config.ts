4061c1e4d44e8dd0d81c5397791de6433a9e1e6e
Benjamin VIELLARD <gbicou@gmail.com>
2023-11-21 16:01:08 +0100

    test: use countries schema only

--- test/fixtures/basic/urql.config.ts
+++ test/fixtures/basic/urql.config.ts
@@ -1,7 +1,9 @@
 import { cacheExchange } from "@urql/core";
 import { executeExchange } from "@urql/exchange-execute";
 import { defineUrqlClient } from "#urql/client";
-import { schema } from "@bicou/countries-server";
+import { buildSchema } from "@bicou/countries-server-schema";
+
+const schema = buildSchema("test");
 
 export default defineUrqlClient((ssr) => ({
   exchanges: [
