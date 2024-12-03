ee6fa5eb5ad8d9c1f6b7b681e3b27b7bb1fbb5c2
Benjamin VIELLARD <gbicou@gmail.com>
2024-12-03 16:14:38 +0100

    chore: upgrade dependencies

--- playground/eslint.config.js
+++ playground/eslint.config.js
@@ -1,4 +1,4 @@
-import * as graphql from '@graphql-eslint/eslint-plugin'
+import graphql from '@graphql-eslint/eslint-plugin'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
@@ -11,19 +11,19 @@ export default withNuxt(
       parser: graphql.parser,
     },
     plugins: {
-      '@graphql-eslint': { rules: graphql.rules },
+      '@graphql-eslint': graphql,
     },
   },
   {
     files: ['**/gql/schema.graphql'],
     rules: {
-      ...graphql.configs['flat/schema-recommended'],
+      ...graphql.configs['flat/schema-recommended'].rules,
       '@graphql-eslint/strict-id-in-types': ['error', { acceptedIdNames: ['id', 'code'] }],
     },
   },
   {
     files: ['**/gql/queries/*.graphql'],
-    rules: graphql.configs['flat/operations-recommended'],
+    rules: graphql.configs['flat/operations-recommended'].rules,
   },
 ).override('nuxt/disables/routes', {
   files: ['**/pages/**/*.vue'],
