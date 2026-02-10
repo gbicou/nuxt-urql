ea1b7993b1de1f9c2b2d4b865033d810bc071dac
Benjamin VIELLARD <gbicou@gmail.com>
2026-02-10 06:09:01 +0100

    style: new lines

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,15 +1,18 @@
 export default defineNuxtConfig({
   modules: ['../src/module', '@nuxt/eslint'],
   css: ['~/assets/css/app.scss'],
+
   experimental: {
     payloadExtraction: false,
   },
+
   eslint: {
     config: {
       stylistic: true,
       tooling: true,
     },
   },
+
   urql: {
     endpoint: 'https://countries.bicou.com/',
   },
